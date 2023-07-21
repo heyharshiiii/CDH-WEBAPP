da121ffb2ecc62f6ac6377a0c9aabad1fb94aaa7
Harshi <harshitaacharya03@gmail.com>
2023-07-21 16:50:29 +0530

    Initialized Firebase Connection

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,25 @@ import 'package:cdh2/NavBar/tabs/CONTENT-TAB/contentscreen.dart';
 import 'package:cdh2/unused/cdh_nav_bar.dart';
 import 'package:cdh2/splashscreen.dart';
 import 'package:cdh2/tab-screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'NavBar/tabs/HOME-TAB/homescreen.dart';
 import 'constants.dart';
 
-void main()
+void main() async
 {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: 
+  FirebaseOptions(
+
+     apiKey: "AIzaSyA5F0P4AUnN2uN9TXV_0zXD2cHitQ_UxsQ",
+    projectId: "cdh-web-a8712",
+    messagingSenderId: "78175276934",
+    appId: "1:78175276934:web:f6e26c704cdcf519e16607",
+    )
+  );
   runApp(MyApp());
 }
 class MyApp extends StatefulWidget {
